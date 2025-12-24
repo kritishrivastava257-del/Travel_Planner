7b87420468560a69b0309f8635899109758b6126
kritishrivastava257-del <kritishrivastava257@gmail.com>
2025-12-24 10:11:57 +0530

    day5 done

--- src/Js/weather.js
+++ src/Js/weather.js
@@ -1,29 +1,35 @@
-const params=new URLSearchParams(window.location.search)
-const city=params.get("city")
-const cityName=document.querySelector("#cityName")
-const loading=document.querySelector("#loading")
-const errorBox=document.querySelector("error")
+const params = new URLSearchParams(window.location.search);
+const city = params.get("city");
+const cityName = document.querySelector("#cityName");
+const loading = document.querySelector("#loading");
+const errorBox = document.querySelector("error");
 
-let API_KEY="129f1a10779aab509c330e108de1e6d6"
+let API_KEY = "129f1a10779aab509c330e108de1e6d6";
 
 if (!city) {
-    loading.classList.add("hidden")
-    errorBox.classList.remove("hidden")
+  loading.classList.add("hidden");
+  errorBox.classList.remove("hidden");
 } else {
-    cityName.textContent=city
+  cityName.textContent = city;
 
-    async function fetchWeather(cityName) {
-        try{
-            const res=await fetch("http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}")
+  async function fetchWeather(cityName) {
+    try {
+      const geoRes = await fetch(
+        `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`
+      );
+      console.log(geoRes);
+      if (!geoRes.ok) throw new Error("Geo API failed");
 
-        }
-        catch(error){
-            console.log(error, "Error in fetching weather details");
-            loading.classList.add("hidden")
-            errorBox.classList.remove("hidden")
-        }
-        
+      const geoData = await geoRes.json();
+      console.log(geoData);
+
+      let { lat, lon } = geoData[0];
+      console.log(lat, lon);
+    } catch (error) {
+      console.log(error, "Error in fetching weather details");
+      loading.classList.add("hidden");
+      errorBox.classList.remove("hidden");
     }
-    fetchWeather(city)
-    
+  }
+  fetchWeather(city);
 }
